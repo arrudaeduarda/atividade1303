f392b3d85ecf16519e8adbec3f773bd6ba23ee52
arrudaeduarda <159277368+arrudaeduarda@users.noreply.github.com>
2024-03-13 11:19:54 -0300

    primeiro commit

---
+++ Atividade01.java
@@ -0,0 +1,16 @@
+package atividade1303;
+
+public class Atividade01 {
+
+	public static void main(String[] args) {
+		 //Variaveis
+		String errado = "tedro tescava teixes no tíer";
+		String corrigirT = errado.replace("t", "P");
+		
+		//Saída ;
+		System.out.println ("Antes:" + errado + " Depois:" + corrigirT);
+		
+		
+				}
+
+}
---
+++ Atividade02.java
@@ -0,0 +1,12 @@
+package atividade1303;
+
+public class Atividade02 {
+
+	public static void main(String[] args) {
+		String frase = "Pedro Pescava Peixes no Píer";
+		String palavra = frase.substring(14, 20);
+		System.out.println("Palavra: " + palavra);
+
+	}
+
+}
